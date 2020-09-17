9974a08fdb4aa3082883a448976f798924340426
SamaraSilva18 <samarasilvacandy@gmail.com>
2020-09-17 12:13:33 +0000

    (17/09) Bubble-Sort com função de comparação

--- bubble-sort.js
+++ bubble-sort.js
@@ -22,6 +22,7 @@ function bubbleSort(vetor) {
     console.log(passadas, comparacoes, totalTrocas)
 }
 
+/*
 let nums = [58, 16, 33, 82, 4, 47, 25, 71, 96, 68, 41, 89]
 
 console.time('Teste 1')
@@ -29,3 +30,20 @@ bubbleSort(nums)
 console.timeEnd('Teste 1')
 
 console.log(nums)
+*/
+
+/*
+const empresas = require('./dados/15-mil-empresas')
+
+console.time('Teste empresas')
+bubbleSort(empresas)
+console.timeEnd('Teste empresas')
+console.log(empresas)
+*/
+
+const nomes = require('./dados/100-mil-nomes')
+
+console.time('Teste nomes')
+bubbleSort(nomes)
+console.timeEnd('Teste nomes')
+console.log(nomes)
